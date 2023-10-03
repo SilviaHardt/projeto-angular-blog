b13244552ca69a663d6c5909fb4400fbf100f8f3
Silvia Hardt <silvinha126@gmail.com>
2023-10-03 12:21:07 -0300

    blog project

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO EM 3D",
+    "description":"Marvel anuncia um novo filme do Homem de Ferro, confira",
+    "photoCover":"https://i.ytimg.com/vi/oXTaxBOP4KQ/maxresdefault.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Nova série anunciada no Disney+",
+    "description":"ola",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
