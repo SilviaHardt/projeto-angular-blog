2f594b495a9940c34e11d1f35e6a7a55263c3475
Silvia Hardt <silvinha126@gmail.com>
2023-10-03 13:55:56 -0300

    traveler blog changes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,30 @@
 export const dataFake = [
   {
     "id":"1",
-    "title":"NOVO HOMEM DE FERRO EM 3D",
-    "description":"Marvel anuncia um novo filme do Homem de Ferro, confira",
-    "photoCover":"https://i.ytimg.com/vi/oXTaxBOP4KQ/maxresdefault.jpg"
+    "title":"Dicas para planejamento de viagem",
+    "description":"Se você tem um forte desejo de viajar ou mesmo só conhecer determinado lugar, comece a poupar e vá em frente! Não demore. Não adie. Perca o medo de viajar. Basta fazê-lo, ou você vai se arrepender quando você for muito ocupado ou muito velho para viajar. Quanto mais flexível você puder ser com seus planos de viagem, mais dinheiro você vai economizar (você será capaz de viajar em horários mais baratos e obter melhores taxas). Use gráficos do Skyscanner para encontrar os dias mais baratos para voar em um determinado mês. Quando se trata de roupas para levar durante a viagem, menos será sempre melhor. Viajar apenas com a bagagem de mão deve ser o seu objetivo. Pense bem: você realmente precisa de três pares de botas, seis calças jeans e um travesseiro? Leve menos e você vai viajar mais barato (não haverá cobranças por bagagem despachada ou por excesso de bagagem), mais rápido (não precisará esperar por suas malas), e mais fácil (uma bolsa significa menos trabalho). É só viajando que você percebe que toda e qualquer sociedade é tão rebelde e plural em seus pontos de vista quanto no seu próprio país. Descubra que no exterior nem tudo são mil maravilhas como nos é contado, e abra os olhos para os pontos positivos do seu próprio país.",
+    "photoCover":"https://content.skyscnr.com/m/77e9f436241cdd58/original/GettyImages-118344741.jpg?crop=1224px:647px&quality=100&position=attention",
+    "link":"https://www.dicasdeviagem.com/"
   },
   {
     "id":"2",
-    "title":"Nova série anunciada no Disney+",
-    "description":"ola",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title":"Atrações para todos os gostos e idades em Serra Negra",
+    "description":"Serra Negra é uma cidade muito mais do que geograficamente estratégica. A montanha, o charme do centrinho de compras, o clima puro e rico em oxigenação, a grande infra-estrutura em hotelaria de lazer e eventos, a gastronomia, a qualidade dos produtos e serviços, a originalidade dos imigrantes italianos, a hospitalidade das pessoas, a beleza natural; enfim, tudo em Serra Negra esta preparado para receber. Os amantes da natureza não podem deixar de fazer um roteiro de agroturismo, pois é nas propriedades rurais entre vales e montanhas que se esconde a paisagem linda de Serra Negra.",
+    "photoCover":"https://www.melhoresdestinos.com.br/wp-content/uploads/2023/07/alto-da-serra-serra-negra-capa.jpg",
+    "link":"https://www.tripadvisor.com.br/Attractions-g675020-Activities-Serra_Negra_State_of_Sao_Paulo.html"
+  },
+  {
+    "id":"3",
+    "title":"O que faz de Brotas um bom passeio",
+    "description":"Turismo de aventura e ecoturismo são os nossos maiores atrativos, mas a cidade reserva muito mais aos visitantes. Além das cachoeiras, rafting, tirolesas e eco parques, exitem pontos turísticos culturais, gastronômicos e hotéis fazenda para viagens em feriados, férias ou fim de semana.Por ser um lugar com natureza muito rica, é claro que o turismo de aventura em Brotas é especialmente famoso e procurado. Mas a cidade tem todo tipo de hotéis, chalés, restaurantes e toda a estrutura que pode agradar grupos de amigos que buscam esportes, como rafting, tirolesa e canionismo (o rapel em cachoeiras), mas também famílias, casais em lua de mel ou em feriados românticos e muito mais. Uma das vantagens de ter Brotas como destino de viagem é o fato de ser um lugar afastado, no meio da natureza, com águas cristalinas, passeios culturais, hotéis fazenda e até eco resorts, sem se afastar demais! Assim, você se afasta do trabalho, da cidade e do estresse do dia a dia sem se preocupar com a complicação e os gastos de viajar para longe. Você nem se lembra que está fazendo turismo no interior de SP!",
+    "photoCover":"https://www.segueviagem.com.br/wp-content/uploads/2020/09/shutterstock_1627888102-1200x675.jpg",
+    "link":"https://www.tripadvisor.com.br/Attractions-g793338-Activities-Brotas_State_of_Sao_Paulo.html"
+  },
+  {
+    "id":"4",
+    "title":"Ilhabela, um dos destinos litorâneos mais procurados",
+    "description":"Ilhabela está entre os destinos litorâneos mais procurados por moradores de todo o estado de São Paulo. Entretanto, nem só de paulistas vive o arquipélago. Ilhabela desperta paixões em turistas de todo o Brasil, que veem no lugar uma ótima oportunidade de aliar conforto, preço acessível, pousadas incríveis e bons serviços. Tudo cercado por excelentes praias. No pacote, entretanto, não podemos deixar de dizer que vem uma pequena pegadinha: os borrachudos. Pois é! Os moradores de Ilhabela costumam dizer que seria perfeição demais se não houvesse os incômodos mosquitinhos. Passado o repelente... É hora de aproveitar!",
+    "photoCover":"https://a.cdn-hotels.com/gdcs/production7/d1330/ab91d370-9eb1-45d7-a7e3-806e2e572354.jpg?impolicy=fcrop&w=800&h=533&q=medium",
+    "link":"https://www.tripadvisor.com.br/Attractions-g609135-Activities-Ilhabela_State_of_Sao_Paulo.html"
   }
 ]
